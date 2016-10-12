4b0eb850d715c3791ceb4b2fbcb43384a38a947c
Eldon Alameda <alameda.eldon@gmail.com>
2016-10-11 23:31:39 -0400

    Completed Tutorial

---
+++ Assets/Scripts/SphereSounds.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereSounds : MonoBehaviour
+{
+
+    AudioSource audioSource = null;
+    AudioClip impactClip = null;
+    AudioClip rollingClip = null;
+
+    bool rolling = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Add audio source component
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialize = true;
+        audioSource.spatialBlend = 1.0f;
+        audioSource.dopplerLevel = 0.0f;
+        audioSource.rolloffMode = AudioRolloffMode.Custom;
+
+        // Load the sphere sounds from Resources
+        impactClip = Resources.Load<AudioClip>("Impact");
+        rollingClip = Resources.Load<AudioClip>("Rolling");
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        // play an impact sound if impacts strong enough
+        if (collision.relativeVelocity.magnitude >= 0.1f)
+        {
+            audioSource.clip = impactClip;
+            audioSource.Play();
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Rigidbody rigid = this.gameObject.GetComponent<Rigidbody>();
+
+        // play the rolling sound
+        if (!rolling && rigid.velocity.magnitude >= 0.01f)
+        {
+            rolling = true;
+            audioSource.clip = rollingClip;
+            audioSource.Play();
+        }
+        else if (rolling && rigid.velocity.magnitude < 0.01f)
+        {
+            rolling = false;
+            audioSource.Stop();
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (rolling)
+        {
+            rolling = false;
+            audioSource.Stop();
+        }
+    }
+}
